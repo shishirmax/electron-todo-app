c117ceb8f01b02b22547b4ee739c53461d6ab795
shishirmax <shishirmax@gmail.com>
2017-02-01 14:31:45 +0530

    Initial Commit - adding new files

---
+++ js/script.js
@@ -0,0 +1,66 @@
+//creating close button
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for(i=0;i<myNodelist.length;i++){
+	var span = document.createElement("SPAN");
+	var txt = document.createTextNode("\u00D7");
+	span.className = "close";
+	span.appendChild(txt);
+	myNodelist[i].appendChild(span);
+}
+
+//hide the current list on clickimng the close button
+var close = document.getElementsByClassName("close");
+var i;
+for(i=0;i<close.length;i++){
+	close[i].onclick = function(){
+		var div = this.parentElement;
+		div.style.display = "none";
+	}
+}
+
+//Adding a checked symbol when clicking on the list item
+
+var list = document.querySelector('ul');
+list.addEventListener('click',function(ev){
+	if(ev.target.tagName === 'LI'){
+		ev.target.classList.toggle('checked');
+	}
+},false);
+
+//Create new list item
+function newElement(){
+	var li = document.createElement("li");
+	var inputValue = document.getElementById("myInput").value;
+	var t = document.createTextNode(inputValue);
+	li.appendChild(t);
+	if(inputValue === ''){
+		alert("Enter some ToDo list...");
+	}else{
+		document.getElementById("myUl").appendChild(li);
+	}
+	document.getElementById("myInput").value = "";
+
+	var span = document.createElement("SPAN");
+	var txt = document.createTextNode("\u00D7");
+	span.className = "close";
+	span.appendChild(txt);
+	li.appendChild(span);
+
+	for(i=0;i<close.length;i++){
+		close[i].onclick = function(){
+			var div = this.parentElement;
+			div.style.display = "none";
+		}
+	}
+}
+//Adding a Digital clock
+function digiClock(){
+		var today = new Date()
+		var time = today.toLocaleTimeString()
+
+		document.getElementById("clock").innerHTML = time
+	}
+	setInterval(function(){
+		digiClock()
+	},1000)
